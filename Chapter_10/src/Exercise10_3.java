ea89bc1466074e53993a925f649ccba23a38c475
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2025-01-27 09:30:21 -0700

    Update Exercise10_3.java

--- Chapter_10/src/Exercise10_3.java
+++ Chapter_10/src/Exercise10_3.java
@@ -1,8 +1,18 @@
 public class Exercise10_3 {
     public static void main(String[] args) {
-        MyInteger num1 = new MyInteger();
-        MyInteger num2 = new MyInteger();
-        System.out.println(num2.isPrime());
 
+        MyInteger num = new MyInteger(-10);
+        
+        // Test methods related to instances
+        System.out.println(num.getValue());
+        System.out.println("Even: " + num.isEven());
+        System.out.println("Odd: " + num.isOdd());
+        System.out.println("Prime: " + num.isPrime());
+        
+        // Test static methods
+        System.out.println();
+        System.out.println("Even: " + MyInteger.isEven(5));
+        System.out.println("Odd: " + MyInteger.isOdd(5));
+        System.out.println("Prime: " + MyInteger.isPrime(5));
     }
 }
