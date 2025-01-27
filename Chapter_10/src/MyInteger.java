ba2412e1b86298197f43a6630cee63a4c6fa4080
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2025-01-27 10:54:43 -0700

    Update MyInteger.java

--- Chapter_10/src/MyInteger.java
+++ Chapter_10/src/MyInteger.java
@@ -16,6 +16,8 @@ class MyInteger {
         return value;
     }
 
+    /// is Even Methods
+
     // is this.value even
     public boolean isEven() {
         if (value % 2 == 0)
@@ -30,6 +32,16 @@ class MyInteger {
         else
             return false;
     }
+    // is instance.getValue even
+    public static boolean isEven(MyInteger instance) {
+        if (instance.getValue() % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    /// isOdd Methods
+
     // is this.value odd
     public boolean isOdd() {
         if (value % 2 == 1)
@@ -44,6 +56,16 @@ class MyInteger {
         else;
             return false;
     }
+    // is instance.getValue odd
+    public static boolean isOdd(MyInteger instance) {
+        if (instance.getValue() % 2 == 1)
+            return true;
+        else;
+            return false;
+    }
+
+    /// isPrime Methods
+
     // is this.value prime
     public boolean isPrime() {
         if (value <= 1)
@@ -71,5 +93,68 @@ class MyInteger {
         }
         
         return false;
+    }
+    // is instance.getValue() prime
+    public static boolean isPrime(MyInteger instance) {
+        if (instance.getValue() <= 1)
+            return false;
+
+        for (int i = 2; i <= instance.getValue(); i++) {
+            if (instance.getValue() % i == 0 && instance.getValue() == i)
+                return true;
+            else if (instance.getValue() % i == 0)
+                return false;
+        }
+        
+        return false;
+    }
+
+    /// equals() methods
+    
+    public boolean equals(int anyIntger) {
+        if (this.value == anyIntger)
+            return true;
+
+        else
+            return false;
+    }
+
+    public boolean equals(MyInteger instance) {
+        if (this.value == instance.getValue())
+            return true;
+
+        else
+            return false;
+    }
+
+
+    /// parseInt() methods
+
+    // Why doesn't this work?
+    // public static int parseInt(char[] charArray) {
+    //     int sum = 0;
+    //     for (int i = 0; i < charArray.length(); i++) {
+    //         sum += (int)charArray[i];
+    //     }
+
+    //     return sum;
+    // }
+
+    public static int parseInt(String s) {
+        int sum = 0;
+        boolean pos = true;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(0) == '\u002D' && i == 0) {
+                pos = false;
+                continue;
+            }
+            sum += (s.charAt(i) - 48) * Math.pow(10, s.length() - i - 1);
+        }
+
+        if (!pos)
+            sum *= -1;
+
+        return sum;
     }
 }
