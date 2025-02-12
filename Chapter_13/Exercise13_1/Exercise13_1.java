fd268062b6e72ab7e8fef6ae7bd88ac56ce5ddaf
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2025-02-12 09:07:34 -0700

    Add files via upload

--- Chapter_13/Exercise13_1/Exercise13_1.java
+++ Chapter_13/Exercise13_1/Exercise13_1.java
@@ -1,5 +1,35 @@
+/*
+Author: Miles Wiser
+Date: 2/12/2025
+
+    This program defines a concrete Triangle class extended from an abstract
+GeometricObject class. Then a Triangle is created given side lengths, color,
+and filled status from the user. Finally, this information is displayed.
+*/
+
+import java.util.Scanner;
+
 public class Exercise13_1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+
+        /** Create triangle with side lengths */
+        System.out.println("Enter three side lengths: ");
+        Triangle myTri = new Triangle(input.nextDouble(), input.nextDouble(), input.nextDouble(),
+        null, false);
+
+        /** Set color and filled */
+        System.out.println("Enter the color of the shape: ");
+        myTri.setColor(input.next());
+
+        System.out.println("Is the shape filled, true/false: ");
+        myTri.setFilled(input.nextBoolean());
+
+        input.close();
+
+        /** Display triangle information */
+        System.out.println(myTri.toString());
+        System.out.println("Area: " + myTri.getArea());
+        System.out.println("Perimeter: " + myTri.getPerimeter());
     }
 }
--- Chapter_13/Exercise13_1/Triangle.java
+++ Chapter_13/Exercise13_1/Triangle.java
@@ -1,3 +1,46 @@
-public class Triangle {
+public class Triangle extends GeometricObject{
+    private double side1;
+    private double side2;
+    private double side3;
+    Triangle() {
+        super();
+    }
 
+    Triangle(double side1, double side2, double side3, String color, boolean filled) {
+        super(color, filled);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    /** Getters */
+    public double getSide1() {
+        return side1;
+    }
+    public double getSide2() {
+        return side2;
+    }
+    public double getSide3() {
+        return side3;
+    }
+
+    /** toString method */
+    @Override
+    public String toString() {
+        return "Triangle: s1 = " + side1 + ", s2 = " + side2 + ", s3 = " + side3
+            + ", " + super.toString();
+    }
+
+    /** getArea method */
+    @Override
+    public double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    /** getPerimeter method */
+    @Override
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
 }
