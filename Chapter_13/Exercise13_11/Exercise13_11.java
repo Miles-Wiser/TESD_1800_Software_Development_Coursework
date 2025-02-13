b6898436c00c8e2f0a667a50d17441988d6ae372
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2025-02-13 10:26:45 -0700

    Add files via upload

---
+++ Chapter_13/Exercise13_11/Exercise13_11.java
@@ -0,0 +1,7 @@
+public class Exercise13_11 {
+    public static void main(String[] args) {
+        Octagon oct1 = new Octagon(5, "Green", true);
+        Octagon oct2 = new Octagon(6, "Green", true);
+
+    }
+}
---
+++ Chapter_13/Exercise13_11/Octagon.java
@@ -0,0 +1,81 @@
+public class Octagon extends GeometricObject
+        implements Comparable<Octagon>, Cloneable {
+    private double side = 0;
+
+    public Octagon() {
+        super();
+    }
+
+    public Octagon(double side, String color, boolean filled) {
+        super(color, filled);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    /** Returns a string representation of this object.
+     * 
+     * @return a string representation of this object.
+     */
+    public String toString() {
+        return "Octagon: side = " + this.side + ", " + super.toString();
+    }
+
+    /**
+     * Returns a {@code double} value for the perimeter of this object. Note:
+     * Does not check for negative values. Can return negative perimeters.
+     * 
+     * @return a double value for this object's perimeter.
+     * 
+     */
+    @Override
+    public double getPerimeter() {
+        return side * 8;
+    }
+
+    /**
+     * Returns a {@code double} value for the area of this object. Note: Does
+     * not check for negative values. Can return negative areas.
+     * 
+     * @return a double value for this object's area.
+     */
+    @Override
+    public double getArea() {
+        return (2 + 4 / Math.sqrt(2)) * side * side;
+    }
+
+    /**
+     * Compares this object's area to object {@code o}'s area. Returns a negative
+     * integer, zero, or positive integer as the area is less than, equal to, or
+     * greater than {@code o}.
+     * 
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     *          is less than, equal to, or greater than {@code o}.
+     */
+    @Override
+    public int compareTo(Octagon o) {
+        if (this.getArea() < o.getArea())
+            return -1;
+        else if (this.getArea() > o.getArea())
+            return 1;
+        else
+            return 0;
+    }
+
+    // /**
+    //  * clone()
+    //  */
+    // @Override
+    // public Octagon clone() throws CloneNotSupportedException {
+    //     try {
+    //         return this.clone();
+    //     }
+    //     catch (CloneNotSupportedException ex) {
+    //         return null;
+    //     }
+    // }
+
+}
