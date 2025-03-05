8b9038627b88a478ba569465d9e1e0665a2dea9b
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2025-03-05 08:41:11 -0700

    Made Exercise17_15 function

--- Chapter_17/Exercise17_14&15/Exercise17_15.java
+++ Chapter_17/Exercise17_14&15/Exercise17_15.java
@@ -1,75 +1,72 @@
+/*
+ * Author: Miles Wiser
+ * Date: 3/5/2025
+ * 
+ *  This program asks the user for a file to encrypt as well as the name of the
+ * files to hold the encryption and the new decrypted data.
+ */
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class Exercise17_15 {
     public static void main(String[] args) throws IOException {
-        File file = new File("Exercise17_14.dat");
+        Scanner input = new Scanner(System.in);
 
-        encryptFile(file);
-        decryptFile(new File("encryptedFile.dat"));
+        // Gather information from user to us in encryptFile() and decryptFile()
+        System.out.println("Enter the name of the file to be encrypted: ");
+        File originalfile = new File(input.next());
+        System.out.println("Enter the name of the encrypted file: ");
+        File encryptedFile = new File(input.next());
+        System.out.println("Enter the name of the decrypted file name: ");
+        File decryptedFile = new File(input.next());
+        input.close();
+
+        encryptFile(originalfile, encryptedFile);
+        decryptFile(encryptedFile, decryptedFile);
     }
 
-    public static void encryptFile(File f) throws IOException {
-        System.out.println("Enter name for encrypted file: ");
-
-        // File nFile = new File("encryptedFile");
-
-        Scanner nameInput = new Scanner(System.in);
-        File nFile = new File(nameInput.next());
-
-        RandomAccessFile nRAF = new RandomAccessFile(f, "r");
-        
+    /// Encrypt Method
+    public static void encryptFile(File inputFile, File outputFile) throws FileNotFoundException, IOException {
         try (
-            RandomAccessFile input = new RandomAccessFile(nFile + ".dat", "rw");
+            RandomAccessFile inputRAF = new RandomAccessFile(inputFile, "r");
+            RandomAccessFile outputRAF = new RandomAccessFile(outputFile, "rw");
         ) {
             int value;
             int count = 0;
 
-            while ((value = (int)(nRAF.read())) != -1) {
-                nRAF.seek(count);
-                input.seek(count);
+            while ((value = (int)(inputRAF.read())) != -1) {
+                inputRAF.seek(count);
+                outputRAF.seek(count);
 
-                input.write(nRAF.read() + (byte)5);
+                outputRAF.write(inputRAF.read() + (byte)5);
 
                 count++;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            nRAF.close();
-        }
+        };
     }
 
-    public static void decryptFile(File f) throws IOException {
-        System.out.println("Enter name for decrypted file: ");
-        Scanner nameInput = new Scanner(System.in);
-        File nFile = new File(nameInput.next());
-
-        // File nFile = new File("decryptedFile");
-        RandomAccessFile nRAF = new RandomAccessFile(f, "r");
-
+    // Exercise17_14.dat    encrypted.dat   decrypted.dat
+    /// Decrypt Method
+    public static void decryptFile(File inputFile, File outputFile) throws FileNotFoundException, IOException {
         try (
-            RandomAccessFile input = new RandomAccessFile(nFile + ".dat", "rw");
+            RandomAccessFile inputRAF = new RandomAccessFile(inputFile, "r");
+            RandomAccessFile outputRAF = new RandomAccessFile(outputFile, "rw");
         ) {
             int value;
             int count = 0;
 
-            while ((value = (int)(nRAF.read())) != -1) {
-                nRAF.seek(count);
-                input.seek(count);
+            while ((value = (int)(inputRAF.read())) != -1) {
+                inputRAF.seek(count);
+                outputRAF.seek(count);
 
-                input.write(nRAF.read() - (byte)5);
+                outputRAF.write(inputRAF.read() - (byte)5);
 
                 count++;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            nRAF.close();
-        }
+        };
     }
 }
